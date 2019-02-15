9e0a2e8f3914e5dc22c47b3d0e944f772d999634
Chirili <chirili503@gmail.com>
2019-02-15 17:41:36 +0100

    added relacion5_ejer5

---
+++ Sistemas/Ejercicios sistemas/relacion5_ejer5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Escribe el nombre de un shell script " script
+
+if [ -e $script ]; then
+    nano $script
+else
+    cat > $script
+    echo "#!/bin/bash" >> $script
+    chmod -x $script
+    nano $script
+fi
+fi
