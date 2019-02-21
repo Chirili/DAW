f9da0b106c6ca076d4856ad09bbd2a358e9b3fac
Chirili <chirili503@gmail.com>
2019-02-21 16:17:45 +0100

    added folder with an exam

---
+++ Sistemas/Examen sistemas 2º Trimestre/andres1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Andrés Carmona Lozano
+read -p "Escribe una letra: " letra
+
+maximo=aa
+if [ $letra \< $maximo ];then
+    resultado=`grep -c "^$letra" /etc/group  resultado`
+    Hay $resultado grupos que empiecen por la letra $letra
+else
+echo "Error: solo se permite una letra"
+fi
