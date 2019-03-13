720d5384ee8e658de7a9d2eb92a88746e7d25461
Chirili <chirili503@gmail.com>
2019-03-13 10:48:00 +0100

    Updated Ej1.java

--- PROGRAMACION/Entornos/Ej1/src/Ej1.java
+++ PROGRAMACION/Entornos/Ej1/src/Ej1.java
@@ -11,7 +11,7 @@ public class Ej1 {
         int num;
 
         do{
-            //Pedimos el primer número
+            //Pedimos el primer n�mero
             num=sn.nextInt();
 
             if(num<=1){
@@ -19,12 +19,12 @@ public class Ej1 {
                         + "Vuelve a intentarlo");
             }
 
-        }while(!(num>1)); //condición para salir
+        }while(!(num<=1)); //condici�n para salir
 
         int suma=0;
 
         //Realizamos la suma
-        for(int contador=1;contador<=num;contador++){
+        for(int contador=1;contador<num;contador++){
             suma+=contador;
         }
 
@@ -32,4 +32,5 @@ public class Ej1 {
 
     }
 
+}
 }
