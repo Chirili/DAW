f8ee842486f0a71bcacfc1bb679c6fc7554a0b98
Chirili <chirili503@gmail.com>
2019-03-22 20:52:26 +0100

    updated calculadora

--- PROGRAMACION/Entornos/Calculadora_Ejercicio3.3/src/Calculadora.java
+++ PROGRAMACION/Entornos/Calculadora_Ejercicio3.3/src/Calculadora.java
@@ -1,3 +1,15 @@
-public class Calculadora {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculadora{
+        int valor = 0;
+        int modoOperacion;
+        int modoCalculadora;
+        int base;
+        int total;
+
+        public void mostrarValor(){
+
+        }
 
 }
